23aa81d832b504d2463b129c14f1dde697105099
unknown <pedrodelemosdamini@gmail.com>
2023-11-28 17:01:27 -0300

    ajustes vendas

--- Venda.py
+++ Venda.py
@@ -57,7 +57,7 @@ def menu_vendas():
         print("Digite os dados dados Venda:")
         idCliente: str = input("id do cliente: ")
         idVendedor: str = input("id co vendedor: ")
-        idProduto: list = input("IDs dos produtos: ")
+        idProduto: list = eval('[' + input("IDs dos produtos: ") + ']')
         valorVenda: str = input("Valor da venda: ")
 
         dados_venda = {
@@ -79,7 +79,7 @@ def menu_vendas():
         print("Digite os dados dados Venda:")
         idCliente: str = input("id do cliente: ")
         idVendedor: str = input("id co vendedor: ")
-        idProduto: list = input("IDs dos produtos: ")
+        idProduto: list = eval('[' + input("IDs dos produtos: ") + ']')
         valorVenda: str = input("Valor da venda: ")
 
         dados_venda = {
@@ -87,24 +87,30 @@ def menu_vendas():
             "idCliente": idCliente,
             "idVendedor": idVendedor,
             "idProduto": idProduto,
-            "valorVenda": valorVenda    
+            "valorvenda": valorVenda    
         }
         return atualiza_venda(dados_venda, id_venda)
 
     elif escolha == "3":
         print("Digite a ID do vendedor que deseja deletar:")
-        id_vendedor = input("ID do vendedor: ")
+        id_venda: str = input("ID do vendedor: ")
         if not id:
             print("nenhum id informado")
             return "nenhum id informado"
-        return deleta_vendedor(id_vendedor)
+        return deleta_venda(id_venda)
     
 
     elif escolha == "4":
-        vendedores = informa_vendedor()
-        print("Lista de vendedores:")
-        for id_vendedor, dados_vendedor in vendedores.items():
-            print(f"ID: {id_vendedor}, nome: {dados_vendedor['nomeVendedor']}")
+        vendas = informa_venda()
+        print("Lista de vendas:")
+        for id_vendas, dados_vendas in vendas.items():
+            pprint(f"ID: {id_vendas}")
+            pprint(f"data da Venda: {dados_vendas['dataVenda']}")
+            pprint(f"id do Cliente: {dados_vendas['idCliente']}")
+            pprint(f"id do Vendedor: {dados_vendas['idVendedor']}") 
+            pprint(f"id dos Produtos: {[dados_vendas['idProduto']]}")
+            print(f"valor da venda: {dados_vendas['valorvenda']}")
+            
         return "vendedores listados"
 
     elif escolha == "0":
